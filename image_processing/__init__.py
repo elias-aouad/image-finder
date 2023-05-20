2509078b881f54751532b7a217ef3ec81a451659
Elias Aouad <eliasaouad97@gmail.com>
2023-05-20 13:18:35 +0200

    changed import style

--- image_processing/__init__.py
+++ image_processing/__init__.py
@@ -1,3 +1,3 @@
-import microsoft_beit
-import resnet_50
-import sift
+import image_processing.microsoft_beit as microsoft_beit
+import image_processing.resnet_50 as resnet_50
+import image_processing.sift as sift
